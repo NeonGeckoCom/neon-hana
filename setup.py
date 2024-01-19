1cf29d3adda8739995b91a95a9a907c560905bc6
Daniel McKnight <daniel@neon.ai>
2024-01-18 19:03:15 -0800

    Update automation for new repo Fix copy/paste leftovers in setup.py

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ def get_requirements(requirements_filename: str):
 with open(path.join(BASE_PATH, "README.md"), "r") as f:
     long_description = f.read()
 
-with open(path.join(BASE_PATH, "hana",
+with open(path.join(BASE_PATH, "neon_hana",
                     "version.py"), "r", encoding="utf-8") as v:
     for line in v.readlines():
         if line.startswith("__version__"):
